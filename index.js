de224a043d494d43a8e3b55fa343e9abe9c8b05d
Michael Henretty <michael.henretty@gmail.com>
2016-02-29 18:53:11 +0000

    adding connection to hue lights

--- index.js
+++ index.js
@@ -1,18 +1,75 @@
-var fs = require('fs');
 var ps = require('pocketsphinx').ps;
+var Microphone = require('mic');
+var concat = require('concat-stream');
+var Hue = require('./lib/hue.js');
 
-modeldir = "../pocketsphinx/model/en-us/"
+function getDecoder() {
+  var modeldir = "../pocketsphinx/model/en-us/";
+  var config = new ps.Decoder.defaultConfig();
+  config.setString("-hmm", modeldir + "en-us");
+  config.setString("-dict", './colors.dic');
+  config.setString("-lm", './colors.lm');
+  config.setString('-logfn', '/dev/null');
+  config.setString("-kws", './colors.kws');
+  return new ps.Decoder(config);
+}
 
-var config = new ps.Decoder.defaultConfig();
-config.setString("-hmm", modeldir + "en-us");
-config.setString("-dict", modeldir + "cmudict-en-us.dict");
-config.setString("-lm", modeldir + "en-us.lm.bin");
-var decoder = new ps.Decoder(config);
+function getMicrophone() {
+  return Microphone({
+    'rate': '16000',
+    'channels': '1',
+    //'debug': true,
+    //'exitOnSilence': 6
+  });
+}
 
-fs.readFile("../pocketsphinx/test/data/goforward.raw", function(err, data) {
-    if (err) throw err;
-    decoder.startUtt();
-    decoder.processRaw(data, false, false);
+var words = [];
+function processWords(newWords) {
+  words = words.concat(newWords.split(' '));
+  console.log('processing words', words);
+  while (words.length > 1) {
+    while (words.length > 0 && words[0] !== 'LIGHTS') {
+      console.log('threw away', words.shift());
+    }
+    if (words.length > 1) {
+      console.log('let there be', words.shift());
+      if (words[0] !== 'LIGHTS') {
+        var color = words.shift();
+        console.log('COMMAND!', color);
+        Hue.setColor(color);
+      }
+    }
+  }
+}
+
+function decode(data) {
+  decoder.processRaw(data, false, false);
+  var hyp = decoder.hyp();
+  if (hyp) {
+    processWords(hyp.hypstr);
+  }
+}
+
+var mic = getMicrophone();
+var stream = mic.getAudioStream();
+buffer = concat(decode);
+
+var decoder = getDecoder();
+decoder.startUtt();
+
+var startTime = Date.now();
+stream.on('data', function (data) {
+  buffer.write(data);
+  if (Date.now() - startTime > 1000) {
+    buffer.end();
+    startTime = Date.now();
+    buffer = concat(decode);
     decoder.endUtt();
-    console.log(decoder.hyp())
+    decoder.startUtt();
+  }
 });
+stream.on('error', function(e) {
+  console.log('error: ' + e);
+});
+
+mic.start();
