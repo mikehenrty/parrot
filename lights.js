9fe79f6cb8a08b6b80f2fcb0da45f08397b10e2c
Michael Henretty <michael.henretty@gmail.com>
2016-03-02 09:01:32 +0000

    cleaing up the api

--- lights.js
+++ lights.js
@@ -1,10 +1,11 @@
 var Commands = require('./lib/commands.js');
 var Hue = require('./lib/hue.js');
 
+var keyword = 'LIGHTS';
 var colors =  [
   'RED', 'BLUE', 'YELLOW', 'GREEN', 'ORANGE'
 ];
-var keyword = 'LIGHTS';
+
 var commands = new Commands(keyword, colors);
 commands.listen(function(command) {
   console.log('got command', command);
